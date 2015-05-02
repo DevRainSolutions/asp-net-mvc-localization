f27db3ff911edc71a1daea277f1dba780278310c
Shevchuk Illya <illyai.shevchuk@gmail.com>
2015-05-02 20:46:09 +0300

    Add User model with DataAnnotations

---
+++ asp-net-mvc-localization/asp-net-mvc-localization/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace asp_net_mvc_localization.Models
+{
+    public class User
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(64)]
+        public string Password { get; set; }
+
+        [Required]
+        [Range(1,125)]
+        public int Age { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+    }
+}
