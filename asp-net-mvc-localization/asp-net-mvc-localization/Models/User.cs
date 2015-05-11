cbbc1cb30e5f70138a2e6c8f439a45b9fd6ef92b
Shevchuk Illya <illyai.shevchuk@gmail.com>
2015-05-11 22:16:25 +0300

    Delete [Display](all work's without it), fix Date format in Create

--- asp-net-mvc-localization/asp-net-mvc-localization/Models/User.cs
+++ asp-net-mvc-localization/asp-net-mvc-localization/Models/User.cs
@@ -10,25 +10,21 @@ namespace asp_net_mvc_localization.Models
         [Required]
         public string Username { get; set; }
 
-        [Display]
         [Required]
         [MyEmailAddress]
         public string Email { get; set; }
-        
-        [Display]
+               
         [Required]
         [MinLength(6)]
         [MaxLength(64)]
         public string Password { get; set; }
-
-        [Display]
+       
         [Range(1,125)]
         public int Age { get; set; }
 
-        [Display]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")] 
         public DateTime Birthday { get; set; }
-
-        [Display]
+       
         [Required]
         [Range(0.1, 9.9)]
         public double Rand { get; set; }
