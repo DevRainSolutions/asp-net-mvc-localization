9eaa1a6f4ed8ec5e989377a59634d68c87349525
Shevchuk Illya <illyai.shevchuk@gmail.com>
2015-05-17 12:11:27 +0300

    ErrorCodeToString refactor

--- asp-net-mvc-localization/asp-net-mvc-localization/Controllers/BaseController.cs
+++ asp-net-mvc-localization/asp-net-mvc-localization/Controllers/BaseController.cs
@@ -21,10 +21,8 @@ namespace asp_net_mvc_localization.Controllers
         public static string ErrorCodeToString(MembershipCreateStatus createStatus)
         {
             Type statusType = createStatus.GetType();
-            Type resourceType = typeof (StatusErrors);
-            return resourceType.GetProperty(
-                statusType.GetEnumName(createStatus)).GetValue(
-                new StatusErrors()).ToString();
+            return StatusErrors.ResourceManager.GetString(
+                statusType.GetEnumName(createStatus));
         }
     }
 }
