6fc08e76ead44aa1889e154fab08af647508564e
Shevchuk Illya <illyai.shevchuk@gmail.com>
2015-06-18 20:43:10 +0300

    StatusErrors rename to MembershipCreateStatusMessages

--- asp-net-mvc-localization/asp-net-mvc-localization/App_GlobalResources/StatusErrors/MembershipCreateStatusMessages.Designer.cs
+++ asp-net-mvc-localization/asp-net-mvc-localization/App_GlobalResources/StatusErrors/MembershipCreateStatusMessages.Designer.cs
@@ -22,14 +22,14 @@ namespace asp_net_mvc_localization {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class StatusErrors {
+    public class MembershipCreateStatusMessages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal StatusErrors() {
+        internal MembershipCreateStatusMessages() {
         }
         
         /// <summary>
@@ -39,7 +39,8 @@ namespace asp_net_mvc_localization {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("asp_net_mvc_localization.App_GlobalResources.StatusErrors.StatusErrors", typeof(StatusErrors).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("asp_net_mvc_localization.App_GlobalResources.StatusErrors.MembershipCreateStatusM" +
+                            "essages", typeof(MembershipCreateStatusMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
